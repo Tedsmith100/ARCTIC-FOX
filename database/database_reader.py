2c294f36ed87c42e131af8077181a6beb9bf4ec9
Tedsmith100 <edward.smith@manchester.ac.uk>
2026-05-18 12:21:01 +0100

    corrct path

--- database/database_reader.py
+++ database/database_reader.py
@@ -1,5 +1,6 @@
 import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from database_functions import read_config_file
 from webserver.sql import SQL
